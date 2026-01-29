afdf27f2e6b53fd633984bbbd8ecd9da126cdfb8
Optima Webdesign <optimawebdesigns12@gmail.com>
2026-01-29 17:48:03 +0530

    test: remove AuthProvider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
-import { AuthProvider } from "@/context/AuthContext";
 import "./globals.css";
-import { Toaster } from "react-hot-toast"; // ✅ Import
 
 export const metadata = {
   title: "EduNexus Global LMS",
@@ -11,11 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-slate-50 text-slate-900">
-        <AuthProvider>
-          {children}
-          {/* ✅ Toast Position set ki */}
-          <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
-        </AuthProvider>
+        {children}
       </body>
     </html>
   );
