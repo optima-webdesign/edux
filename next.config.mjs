513f72740dfe3f53bc206d9e02641d7b5d865b43
Optima Webdesign <optimawebdesigns12@gmail.com>
2026-01-29 17:50:39 +0530

    fix routing and next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ui-avatars.com', // ✅ Yeh line add karni hai
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc', // ✅ Yeh bhi add kar lo safety ke liye
-        pathname: '/**',
-      },
-    ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
   },
 };
 
